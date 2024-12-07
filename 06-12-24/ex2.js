17c549d0cc60f4d1e81b122e932be29e72b2b838
Gabiss1 <136657181+Gabiss1@users.noreply.github.com>
2024-12-06 22:02:09 -0300

    Add files via upload

---
+++ 06-12-24/ex2.js
@@ -0,0 +1,17 @@
+/******************************************************************************
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+let idade
+idade = 15
+let status = idade>= 18? 'adulto':'menor de idade'
+
+console.log(status)
+
+let a = 'alfa'
+let b = 'beto'
+
+console.log(a+b)
---
+++ 06-12-24/main.js
@@ -0,0 +1,28 @@
+/******************************************************************************
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+console.log('Hello World');
+
+let a = true
+let b = false
+let resultado = a && b
+
+console.log(a, '&&', b, '=', resultado)
+console.log(`${!a}`)
+console.log(`${a || b}`)
+
+let num1 = 8
+let num2 = 4
+
+console.log(`${num1} e ${num2} são iguais? ${num1===num2}`)
+console.log(`${num1} e ${num2} não são iguais? ${num1!==num2}`)
+console.log(`${num1} é maior que ${num2}? ${num1>num2}`)
+console.log(`${num1} é menor que ${num2}? ${num1<num2}`)
+console.log(`${num1} é igual ou maior que ${num2}? ${num1>=num2}`)
+console.log(`${num1} é igual ou menor que ${num2}? ${num1<=num2}`)
+
+
